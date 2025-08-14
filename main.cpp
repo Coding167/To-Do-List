6921560817515a8eb9f602942f15ccccca885911
Coding <m64513824@gmail.com>
2025-08-14 13:00:12 +0300

    Creating Delete Task Feature

--- main.cpp
+++ main.cpp
@@ -3,6 +3,41 @@
 #include "file.h"
 using namespace std;
 
+vector<string> split(const string& text, char c) {
+    vector<string> result;
+    string current;
+
+    for (char ch : text) {
+        if (ch == c) {
+            result.push_back(current);
+            current.clear();
+        } else {
+            current += ch;
+        }
+    }
+
+    result.push_back(current);
+    return result;
+}
+
+int validateChoice(int start, int end, const string& message) {
+    int choice;
+    do {
+        cout<<message;
+        cin>>choice;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input! Please enter a number between "<<start<<" and "<<end<<".\n";
+            choice = start - 1;
+        } 
+        else if (choice < 1 || choice > 5) {
+            cout << "Choice out of range! Please enter "<<start<<" to "<<end<<".\n";
+        }
+    }while(choice > end || choice < start);
+    return choice;
+}
+
 int main() {
     cout<<"==== Welcome to our To-Do list app ====\n";
 
@@ -12,23 +47,10 @@ int main() {
             <<"3. Mark task as complete\n"
             <<"4. Delete task\n"
             <<"5. Exit\n";
-        int choice;
-        // Validate Choice
-        do {
-            cout<<"Enter your choice: ";
-            cin>>choice;
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                cout << "Invalid input! Please enter a number between 1 and 5.\n";
-                choice = 0;
-            } 
-            else if (choice < 1 || choice > 5) {
-                cout << "Choice out of range! Please enter 1 to 5.\n";
-            }
-        }while(choice > 5 || choice < 1);
+        int choice = validateChoice(1,5,"Enter Your Choice: ");
         
         // Features
+        File file("data.csv");
         if (choice == 1) {
             cout<<"Add new task Feature.\n";
         }else if (choice == 2) {
@@ -36,7 +58,21 @@ int main() {
         }else if (choice == 3) {
             cout<<"Mark task as Complate Feature.\n";
         }else if (choice == 4) {
-            cout<<"Delete task Feature.\n";
+            vector<string> tasks = file.read();
+            tasks.erase(tasks.begin());
+            if (tasks.empty()) {
+                cout<<"You haven't any task.\n";
+            }else {
+                for (int i = 0 ; i < tasks.size() ; i++) {
+                    vector<string> parts = split(tasks.at(i),',');
+                    cout<<i+1<<". "<<parts.at(0)<<" - "<<(parts.at(1) == "1" ? "Complated" : "Not Complated")<<endl;
+                }
+                int c = validateChoice(0,tasks.size(), "Enter the task to delete it [0 to exit]: ");
+                if (c != 0) {
+                    cout<<c<<endl;
+                    file.removeLine(c+1);
+                }
+            }
         }else {
             break;
         }
